7260ba292e7528b370bd3b40a10b40c0769d2249
Vinod Sagar <you@example.com>
2023-04-10 16:56:08 +0530

    added some nginx333

--- nginx.sh
+++ nginx.sh
@@ -6,8 +6,6 @@ chmod 710 /var/lib/jenkins/workspace/django-cicd
 sudo ln -s /etc/nginx/sites-available/jenkinspro /etc/nginx/sites-enabled
 sudo nginx -t
 
-sudo nginx -t
-
 sudo systemctl start nginx
 sudo systemctl enable nginx
 

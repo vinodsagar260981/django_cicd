e58656b7490a109349621f759e7be06b34a9f9fe
Vinod Sagar <you@example.com>
2023-04-10 13:08:48 +0530

    added some file11

--- nginx.sh
+++ nginx.sh
@@ -3,6 +3,9 @@
 sudo cp -rf jenkinspro.conf /etc/nginx/conf.d
 chmod 710 /var/lib/jenkins/workspace/django-cicd
 
+sudo ln -s /etc/nginx/sites-available/jenkinspro /etc/nginx/sites-enabled
+sudo nginx -t
+
 sudo nginx -t
 
 sudo systemctl start nginx
